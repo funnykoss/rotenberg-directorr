b501ffe6f01270812097ac78238d71e1da035be1
funnykoss <75586141+funnykoss@users.noreply.github.com>
2023-03-16 17:09:54 +0100

    button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,13 @@
+import Button from "../Button";
+
 const Hero = () => {
-  return <></>;
+  return (
+    <>
+      <p>Lorem ipsum dolor</p>
+      <h1> Name </h1>
+      <p>description</p>
+      <Button></Button>
+    </>
+  );
 };
 export default Hero;
